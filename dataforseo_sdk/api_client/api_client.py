b183e79b36cc403d3dddbde9c8600b51299fe667
Dan Schaefer <danschaefer@gmail.com>
2022-02-25 15:49:24 -0500

    api client now saves all responses to file

--- dataforseo_sdk/api_client/api_client.py
+++ dataforseo_sdk/api_client/api_client.py
@@ -1,10 +1,17 @@
+import os
+
 from .rest_client import RestClient
+
+DEFAULT_DATA_DIR = os.path.realpath(
+    os.path.join(os.path.dirname(os.path.realpath(__file__)), "..", "_data")
+)
 
 
 class APIClient:
-    def __init__(self, credentials, api_version="v3"):
+    def __init__(self, credentials, api_version="v3", data_dir=None):
         self.credentials = credentials
         self.api_version = api_version
+        self.data_dir = data_dir or DEFAULT_DATA_DIR
 
         self._client = None
 
@@ -12,7 +19,9 @@ class APIClient:
     def client(self):
         if not self._client:
             self._client = RestClient(
-                self.credentials.username, self.credentials.password
+                self.credentials.username,
+                self.credentials.password,
+                requests_log_dir=self.data_dir,
             )
         return self._client
 
