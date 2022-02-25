85fe6ff5da7961b9ec002ae524c0c66bd56bc850
Dan Schaefer <danschaefer@gmail.com>
2022-02-25 15:07:21 -0500

    feature: save response as a file

--- dataforseo_sdk/api_client/rest_client.py
+++ dataforseo_sdk/api_client/rest_client.py
@@ -1,7 +1,15 @@
+import datetime
+import os
+import re
+
 from http.client import HTTPSConnection
 from base64 import b64encode
 from json import loads
 from json import dumps
+
+from slugify import slugify
+
+ALLOWED_SLUG_CHARS = re.compile(r'[^-_a-zA-Z0-9]+')
 
 
 class RestClient:
@@ -10,9 +18,23 @@ class RestClient:
 
     domain = "api.dataforseo.com"
 
-    def __init__(self, username, password):
+    def __init__(self, username, password, requests_log_dir=None):
         self.username = username
         self.password = password
+        self.requests_log_dir = requests_log_dir
+
+    def _generate_file_path_for_request(self, endpoint):
+        now = datetime.datetime.now()
+        date_str = now.strftime("%Y%m%dT%H%M%S.%f")
+        file_name = f"{self.domain}.{slugify(endpoint, regex_pattern=ALLOWED_SLUG_CHARS)}.{date_str}.json"
+        return os.path.join(self.requests_log_dir, file_name)
+
+
+    def _maybe_create_response_file(self, endpoint, response_json):
+        if self.requests_log_dir:
+            file_path = self._generate_file_path_for_request(endpoint=endpoint)
+            with open(file_path, "w+") as fh:
+                fh.write(response_json)
 
     def request(self, path, method, data=None):
         connection = HTTPSConnection(self.domain)
@@ -23,7 +45,10 @@ class RestClient:
             headers = {'Authorization' : 'Basic %s' %  base64_bytes, 'Content-Encoding' : 'gzip'}
             connection.request(method, path, headers=headers, body=data)
             response = connection.getresponse()
-            return loads(response.read().decode())
+            response_json = response.read().decode()
+            self._maybe_create_response_file(path, response_json)
+            response_object = loads(response_json)
+            return response_object
         finally:
             connection.close()
 
---
+++ tests/api_client/test_rest_client.py
@@ -0,0 +1,62 @@
+from http.client import HTTPResponse
+import os
+import sys
+from unittest import TestCase
+from unittest.mock import MagicMock, Mock, patch
+
+from dataforseo_sdk.api_client.rest_client import RestClient
+
+class TestRestClient(TestCase):
+
+    def setUp(self) -> None:
+        self.data_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), "data")
+        self.response_file = None
+        return super().setUp()
+
+    def tearDown(self) -> None:
+        # if self.response_file is not None and os.path.isfile(self.response_file):
+        #     os.remove(self.response_file)
+        return super().tearDown()
+
+    def setup_mocking(self, mock_https_connection_class, response_json):
+        self.mock_http_response = Mock(spec=HTTPResponse)
+        self.mock_http_response.read.return_value = response_json
+        self.mock_https_connection = MagicMock()
+        self.mock_https_connection.getresponse.return_value = self.mock_http_response
+        mock_https_connection_class.return_value = self.mock_https_connection
+
+
+    @patch("dataforseo_sdk.api_client.rest_client.HTTPSConnection")
+    def test_request(self, mock_https_connection_class):
+        response_json = b'{"test": "message"}'
+        expected_response = {"test": "message"}
+        self.setup_mocking(mock_https_connection_class, response_json)
+        endpoint = "/v3/my_endpoint"
+
+        rest_client = RestClient("username", "password")
+        response = rest_client.request(endpoint, "GET")
+
+        assert response == expected_response
+
+
+    @patch("dataforseo_sdk.api_client.rest_client.datetime")
+    @patch("dataforseo_sdk.api_client.rest_client.HTTPSConnection")
+    def test_request__creates_file(self, mock_https_connection_class, mock_datetime):
+        response_json = b'{"test": "message"}'
+        date_str = "20220225T084125.123456"
+        expected_domain_name = "api.example.com"
+        self.setup_mocking(mock_https_connection_class, response_json)
+        mock_now = MagicMock()
+        mock_now.strftime.return_value = date_str
+        mock_datetime.datetime.now.return_value = mock_now
+        endpoint = "/v3/my_endpoint"
+        slug_endpoint = "v3-my_endpoint"
+        self.response_file = os.path.join(self.data_dir, f"{expected_domain_name}.{slug_endpoint}.{date_str}.json")
+
+        rest_client = RestClient("username", "password", requests_log_dir=self.data_dir)
+        rest_client.domain = expected_domain_name
+        rest_client.request(endpoint, "GET")
+
+        assert os.path.isfile(self.response_file)
+        with open(self.response_file, "r") as f:
+            assert f.read() == response_json.decode()
